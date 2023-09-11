99ea6e403baec30b3be380f28defaca118717d0e
Jalal <110737334+Jalalbaim@users.noreply.github.com>
2023-09-11 12:02:30 +0200

    .py

---
+++ K means algorithm.py
@@ -0,0 +1,32 @@
+## K means algorithm
+
+import numpy as np
+
+# distance euclidienne
+
+def euclidean_distance (x1,x2):
+  return np.sqrt(np.sum((x1 - x2)**2))
+
+# Algorithm
+max_iterations = 100
+
+def kmeans (data, k):
+    # initialisation aléatoire des centroids
+    centroids = data[:, np.random.choice(range(data.shape[1]), k, replace=False)].T
+
+    for i in range(100):
+      # association de chaque point à un cluster
+        clusters = [[] for _ in range(k)]
+        for point in data.T:
+            distances = [euclidean_distance(point, centroid) for centroid in centroids]
+            cluster_idx = np.argmin(distances)
+            clusters[cluster_idx].append(point)
+
+      # associer de nouveau centroids au median des point à chaque cluster
+        new_centroids = [np.mean(cluster, axis=0) if cluster else centroid for cluster, centroid in zip(clusters, centroids)]
+
+      # Check pour la convergence
+        if np.all(np.array(new_centroids) == np.array(centroids)):
+          break
+        centroids = new_centroids
+    return centroids, clusters
